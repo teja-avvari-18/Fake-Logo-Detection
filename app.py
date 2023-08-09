9d4b0deae438abc7e221b10ad90038b6a294ee1c
teja-avvari-18 <avvariteja2001@gmail.com>
2023-08-09 20:06:59 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,63 @@
+
+from __future__ import division, print_function
+# coding=utf-8
+import sys
+import os
+import glob
+import re
+import numpy as np
+
+from keras.applications.imagenet_utils import preprocess_input, decode_predictions
+from keras.models import load_model
+from keras.preprocessing import image
+
+from flask import Flask, redirect, url_for, request, render_template
+from werkzeug.utils import secure_filename
+from gevent.pywsgi import WSGIServer
+
+app = Flask(__name__)
+
+MODEL_PATH = "C:/Users/avvar/FL Detection/models/fldetection.h5"
+model = load_model(MODEL_PATH)
+
+
+print('Model loaded. Check http://127.0.0.1:5000/')
+
+
+def model_predict(img_path, model):
+    img = image.load_img(img_path, target_size=(256, 256))
+    x = image.img_to_array(img)
+    x = np.expand_dims(x, axis=0)
+    x = preprocess_input(x)
+    preds = model.predict(x)
+    return preds
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        # Get the file from post request
+        f = request.files['file']
+
+        # Save the file to ./uploads
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(
+            basepath, 'uploads', secure_filename(f.filename))
+        f.save(file_path)
+
+        # Make prediction
+        preds = model_predict(file_path, model)
+        if preds > 0.5:
+            result = 'The logo is fake'
+        else:
+            result = 'The logo is real'
+        return result
+    return None
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
